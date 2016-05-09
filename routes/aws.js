d410ddfd4e6d166e97d92c4c34ae2163c3e849dc
dretnan <d.retnan@nhubnigeria.com>
2016-05-09 19:37:11 +0100

    added to route

--- routes/aws.js
+++ routes/aws.js
@@ -5,7 +5,7 @@ AWS.config.region = 'us-west-2';
 var s3 = new AWS.S3();
 
 /* GET users listing. */
-router.get('/aws/sign_request', function(req, res, next) {
+router.get('/sign_request', function(req, res, next) {
 	var params = {Bucket: 'logicaladdress', Key: 'myKey', Expires: 86400};
 	s3.getSignedUrl('putObject', params, function (err, url) {
 		res.status(200);
