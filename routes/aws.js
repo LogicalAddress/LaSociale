6e561aa92b0f7eecf4d2119a469baa01b458e5e3
dretnan <d.retnan@nhubnigeria.com>
2016-05-09 19:31:37 +0100

    aws s3 route

---
+++ routes/aws.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-west-2';
+var s3 = new AWS.S3();
+
+/* GET users listing. */
+router.get('/aws/sign_request', function(req, res, next) {
+	var params = {Bucket: 'logicaladdress', Key: 'myKey', Expires: 86400};
+	s3.getSignedUrl('putObject', params, function (err, url) {
+		res.status(200);
+		return res.json({singedUrl: url});
+	});
+});
+
+module.exports = router;
