b1c06f4dcf7fae921b59d96d37b79d4dd82dee17
dretnan <d.retnan@nhubnigeria.com>
2016-05-18 06:49:00 +0100

    signedurl

--- routes/aws.js
+++ routes/aws.js
@@ -3,13 +3,25 @@ var router = express.Router();
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
 var s3 = new AWS.S3();
+var uuid = require('uuid');
+
+/*
+* Preset in ENV
+* AWSAccessKeyId
+* AWSSecretKey
+*/
 
 /* GET users listing. */
 router.get('/sign_request', function(req, res, next) {
-	var params = {Bucket: 'logicaladdress', Key: 'myKey', Expires: 86400};
+	var params = {
+		Bucket: 'lasociale-static', 
+		Key: 'uploads/'+uuid.v4()+'/${filename}',
+		ACL: 'public-read',
+		Expires: 86400
+	};
 	s3.getSignedUrl('putObject', params, function (err, url) {
 		res.status(200);
-		return res.json({singedUrl: url});
+		return res.json({signedUrl: url});
 	});
 });
 
