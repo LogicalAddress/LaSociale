07bfb6d4f7f748d62d148f33b34d145c3b1337e5
dretnan <d.retnan@nhubnigeria.com>
2016-05-18 12:45:02 +0100

    fixing

--- routes/aws.js
+++ routes/aws.js
@@ -8,7 +8,7 @@ var s3 = new AWS.S3({
 	region: 'eu-central-1',
 	signatureVersion: 'v4',
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-	secretAccessKey process.env.AWS_SECRET_ACCESS_KEY || '',
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
 });
 
 /* GET sign a url */
